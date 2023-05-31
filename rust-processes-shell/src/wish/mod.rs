d35372aa3821a18bdf7907b27f00fdf409165246
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2023-05-31 20:44:59 +0900

    execute_line을 분리함

--- rust-processes-shell/src/wish/mod.rs
+++ rust-processes-shell/src/wish/mod.rs
@@ -2,12 +2,46 @@ pub struct Wish;
 
 use std::io::{self, Write};
 
+enum ExecutionResult {
+    Continue(u8),
+    Exit,
+}
+
 impl Wish {
     pub fn new() -> Self {
         Wish
     }
 
-    pub fn run(&self) {
-        println!("not yet");
+    pub fn run(&self) -> u8 {
+        loop {
+            print!("wish> ");
+            io::stdout().flush().unwrap();
+
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+
+            match self.execute_line(&input) {
+                ExecutionResult::Exit => break 0,
+                ExecutionResult::Continue(code) => {
+                    if code != 0 {
+                        println!("wish: command failed with exit code {}", code);
+                        break code;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+
+    fn execute_line(&self, cmd: &str) -> ExecutionResult {
+        let cmd = cmd.trim();
+        println!("You entered: \"{}\"", cmd);
+        if cmd == "exit" {
+            println!("Exiting...");
+            ExecutionResult::Exit
+        } else {
+            ExecutionResult::Continue(0)
+        }
     }
 }
