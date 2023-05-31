50826535facc714ce83b4d2e32500d74e093d1ec
Kang Hyunwoong <hyunwoong.kang@leedotype.com>
2023-05-31 20:37:22 +0900

    uninstalled libc

--- rust-processes-shell/src/wish/mod.rs
+++ rust-processes-shell/src/wish/mod.rs
@@ -2,28 +2,12 @@ pub struct Wish;
 
 use std::io::{self, Write};
 
-fn flush_stdout() {
-    io::stdout().flush().expect("Cannot print to stdout");
-}
-
 impl Wish {
     pub fn new() -> Self {
         Wish
     }
 
     pub fn run(&self) {
-        loop {
-            print!("wish> ");
-            //flush stdout
-            flush_stdout();
-
-            let mut input = String::new();
-            std::io::stdin().read_line(&mut input).unwrap();
-            let input = input.trim();
-            if input == "exit" {
-                break;
-            }
-            println!("You entered: {}", input);
-        }
+        println!("not yet");
     }
 }
